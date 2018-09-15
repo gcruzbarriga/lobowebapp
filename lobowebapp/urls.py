e39b84fec52dbcdda783ef9a0f121118f7dfe1f0
gcruzbarriga <gcruzbarriga@gmail.com>
2018-09-15 15:46:47 -0500

    V1.0.1
    Corrección de URLs

--- lobowebapp/urls.py
+++ lobowebapp/urls.py
@@ -4,11 +4,11 @@ from control import views
 
 urlpatterns = [
     url(r'^$', views.index, name="home"),
-    url('comida', views.comida, name="comida"),
-    url('confirmar', views.confirmacion, name="confirmacion"),
-    url('confirmarComida', views.confirmacionComida, name="confirmacionComida"),
-    url('reporte', views.reporte, name="reporte"),
-    url('export/csv/', views.export_users_csv, name="export_users_csv"),
+    url(r'^comida/$', views.comida, name="comida"),
+    url(r'^confirmar/$', views.confirmacion, name="confirmacion"),
+    url(r'^confirmarComida/$', views.confirmacionComida, name="confirmacionComida"),
+    url(r'^reporte/$', views.reporte, name="reporte"),
+    url(r'^export/csv/', views.export_users_csv, name="export_users_csv"),
     
     url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^admin/', admin.site.urls),
